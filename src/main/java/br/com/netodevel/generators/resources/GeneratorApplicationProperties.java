0982cb2a8d0bf1531d4ee2ad0b1e1f6141230ce4
JoseVieira <josevieira.dev@gmail.com>
2017-09-16 17:53:03 -0300

    added generator application.properties

--- src/main/java/br/com/netodevel/generators/resources/GeneratorApplicationProperties.java
+++ src/main/java/br/com/netodevel/generators/resources/GeneratorApplicationProperties.java
@@ -1,5 +1,7 @@
 package br.com.netodevel.generators.resources;
 
+import java.io.IOException;
+
 import br.com.netodevel.core.GeneratorOptions;
 import br.com.netodevel.core.GeneratorResource;
 
@@ -23,8 +25,30 @@ public class GeneratorApplicationProperties extends GeneratorResource {
 
 	@Override
 	protected String operationGenerate(String javaStrings, String database, String orm, String server) {
-		System.out.println("ORM: " + this.generatorOptions.getOrm());
-		return null;
+		/**
+		 * TODO: factory to all orms supported
+		 */
+		if(orm.equals("jpa")) {
+			javaStrings = javaStrings.replace("${jpa_config}", ConfigProps.JPA_CONFIG);
+		}
+		
+		/**
+		 * TODO: factory to all server supported
+		 */
+		if (server.equals("tomcat")) {
+			javaStrings = javaStrings.replace("${server_config}", ConfigProps.TOMCAT_CONFIG);
+		}
+		
+		try {
+			javaStrings = javaStrings.replace("${database_name}", getNameDatabase())
+					.replace("${database}", database)
+					.replace("${user_database}", getUserDatabase())
+					.replace("${password_database}", getPassWordDatabase());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return javaStrings;
 	}
 	
 	public static void main(String[] args) {
@@ -32,6 +56,7 @@ public class GeneratorApplicationProperties extends GeneratorResource {
 				.setDatabase("mysql")
 				.setOrm("jpa")
 				.setServer("tomcat");
+		
 		new GeneratorApplicationProperties(options).generate("application.properties");
 	}
 	
